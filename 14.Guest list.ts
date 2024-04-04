27a280dd701fc9199629868fed14e8d31c8e9e57
Mubashir915 <159889709+Mubashir915@users.noreply.github.com>
2024-04-04 16:53:02 +0500

    Add files via upload

---
+++ 14.Guest list.ts
@@ -0,0 +1,4 @@
+let guest_list : string[]= [`Tariq`,`Ali`,`Shammas`];
+for(let i=0; i<guest_list.length; i++){
+    console.log(`Respected Sir` + guest_list[i] + `,\nWe are iniviting you on dinner tomorrow,\nThank You`);
+}
