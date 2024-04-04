b46a89fec1838a8e0995acd6c54bace1d7c2c109
Mubashir915 <159889709+Mubashir915@users.noreply.github.com>
2024-04-04 16:52:32 +0500

    Add files via upload

---
+++ 13.Transportation.ts
@@ -0,0 +1,5 @@
+// Exercise 13
+let transportation : string [] = [`civic`,`bike`,`bus`,`shehzore`];
+for(let i=0; i<transportation.length; i++){
+    console.log(`I Would Like To Own a ` + transportation[i]);
+}
