f9af3f6d525105eed4744a80faa4431e71b5a415
Mubashir915 <159889709+Mubashir915@users.noreply.github.com>
2024-04-04 16:51:51 +0500

    Add files via upload

---
+++ 11.Names.ts
@@ -0,0 +1,13 @@
+// Store the names of a few of your friends in a array called names.
+//  Print each person’s name by accessing each element in the list, one at a time.
+
+
+
+
+ 
+// EXERCISE
+let members: string[] =['Mubashir','Tariq','ALi','Shammas'];
+let message: string= 'Kal University jana hy  ywr:';
+for(let i=0;  i<members.length; i++){
+  console.log(message + members[i]);
+}
