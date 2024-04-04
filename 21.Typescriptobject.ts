b8b4348c17da24011f83817967be228657a08047
Mubashir915 <159889709+Mubashir915@users.noreply.github.com>
2024-04-04 16:54:47 +0500

    Add files via upload

---
+++ 21.Typescriptobject.ts
@@ -0,0 +1,15 @@
+interface item {
+    name:string
+    Number:
+}
+//  create two objects
+const book : item ={
+    name: 'Essential  Typescript'
+    price:999
+}
+const apple : item ={
+    name='Apple'
+    price:200
+}
+console.log(`book name: ${book.name}, Price: ${book.price}`)
+console.log(`book name: ${book.name}, Price: ${book.price}`)
