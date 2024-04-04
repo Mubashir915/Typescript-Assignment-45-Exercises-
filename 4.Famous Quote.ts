59c9d320aaf6189b07f29372815d64b85a092ffc
Mubashir915 <159889709+Mubashir915@users.noreply.github.com>
2024-04-04 16:47:54 +0500

    Add files via upload

---
+++ 4.Famous Quote.ts
@@ -0,0 +1,8 @@
+//Find a quote from a famous person you admire. Print the quote and the name of its author.
+//  Your output should look something like the following, including the quotation marks:
+ let quote: string="A person who never made a mistake never tried anything new."
+ let author="Albert Einstein";
+ console.log(`${author} once said "${quote}`);
+ 
+
+
