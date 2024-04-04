e2feb1289d282cd091d26e2f0445b4e13899fbee
Mubashir915 <159889709+Mubashir915@users.noreply.github.com>
2024-04-04 16:51:14 +0500

    Add files via upload

---
+++ 10.Adding Folder.ts
@@ -0,0 +1,11 @@
+// 10.Choose two of the programs you’ve written, and add at least one comment to each.
+//  If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file.
+//  Then write one sentence describing what the program does.
+
+
+
+//Exercise
+//My name is Muhammad Mubashir
+//Dated: 21/02/2024
+//this program will run simple code just like hello world
+console.log("hello world simple program");
